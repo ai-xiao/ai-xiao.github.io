9720e1298952df2da39211bee9dc664dbcb0e1f1
qc670980756 <42692108@qq.com>
2020-12-10 13:33:17 +0800

    文档初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    serviceWorker: true,
+    title: '个人主页',
+    description: 'Not Every',
+    head: [
+        ['link', { rel: 'icon', href: '/hero.ico' }],
+	    ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['link', { rel: 'apple-touch-icon', href: '/hero.png' }],
+    ],
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博文',
+              items: [
+                { text: 'Android', link: '/android/' },
+                { text: 'ios', link: '/ios/' },
+                { text: 'Web', link: '/web/' }
+              ]
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://github.com/ai-xiao' },
+        ],
+        sidebar: {
+            '/android/': [
+                        "",
+                        "android1",
+                         ],
+                "/ios/":[
+                        "",
+                        "ios1",
+                        ],
+                "/web/":[
+                        "",
+                        "web1",
+                        ],
+            },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+    },
+}
